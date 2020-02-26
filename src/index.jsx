62e1ff95a04de8de3a366e065422156199bd09ff
Goor Lavi <GoorLavi1@Gmail.com>
2020-02-26 19:36:20 +0200

    Update index.jsx
    bug fix from source repo

--- src/index.jsx
+++ src/index.jsx
@@ -283,8 +283,9 @@ class LazyLoad extends Component {
     return this.visible ?
            this.props.children :
              this.props.placeholder ?
-                this.props.placeholder :
-                <div style={{ height: this.props.height }} className="lazyload-placeholder" />;
+                    <div ref={this.setRef}>{this.props.placeholder}</div> :
+                     <div style={{ height: this.props.height }} className="lazyload-placeholder" ref={this.setRef} />;
+
   }
 }
 
